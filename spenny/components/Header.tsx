e415b89aeeaad304220ae3fd6dab5c15e1185e14
adrians90 <adriansandu9090@gmail.com>
2024-07-23 12:31:03 +0100

    checkUser function - saves the user in the database

--- spenny/components/Header.tsx
+++ spenny/components/Header.tsx
@@ -1,6 +1,8 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
-const Header = () => {
+import { checkUser } from "@/lib/checkUser";
+const Header = async () => {
+  const user = await checkUser();
   return (
     <nav className="navbar">
       <div className="navbar-container">
