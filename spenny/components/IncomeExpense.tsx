7519d1a93ce17ef15d644538ac674138819b6678
adrians90 <adriansandu9090@gmail.com>
2024-07-23 14:06:05 +0100

    refactor: Format balance and income/expense amounts with two decimal places

--- spenny/components/IncomeExpense.tsx
+++ spenny/components/IncomeExpense.tsx
@@ -6,11 +6,11 @@ const IncomeExpense = async () => {
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">£{income}</p>
+        <p className="money plus">£{income?.toFixed(2)}</p>
       </div>
       <div>
         <h4>Expenses</h4>
-        <p className="money minus">£{expense}</p>
+        <p className="money minus">£{expense?.toFixed(2)}</p>
       </div>
     </div>
   );
